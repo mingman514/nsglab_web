5632c369f7afa489e33ffe297c2756db4bd9afb8
mingman <mingyuchoi514@gmail.com>
2023-06-20 10:40:24 +0900

    cors update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config({path: '../../.env'});
+console.log("path", __dirname)
 
 const env = process.env.NODE_ENV;
 const express = require('express');
@@ -6,15 +7,21 @@ const app = express();
 
 const cors = require('cors');
 
-if (env === "production"){
-  app.use(cors({
-    origin: 'https://netsys.skku.edu'     // 교차출처 리소스 공유 허용 URL
-  }));
-} else {
-  app.use(cors({
-    origin: 'http://localhost:3000'
-  }));
-}
+const whiteList = ["https://netsys.skku.edu", "http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) { 
+    if (whiteList.indexOf(origin) !== -1) { // 만일 whitelist 배열에 origin인자가 있을 경우
+      callback(null, true); // cors 허용
+    } else {
+      callback(new Error("Not Allowed Origin!")); // cors 비허용
+    }
+  },
+};
+app.use(cors(corsOptions));
+
+// app.use(cors({
+//   origin: '*',
+// }));
 
 // Connect to MongoDB
 const connect = require('./schemas')
@@ -32,5 +39,6 @@ app.use('/content', contentRouter);
         
 const port = process.env.PORT;
 app.listen(port, () => {
+    console.log("Node Env:", process.env.NODE_ENV);
     console.log(`server is listening at localhost:${port}`);
 });
